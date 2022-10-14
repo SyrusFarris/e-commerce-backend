17c192499f1ba2c35c6bf1aec5ba767d7f700458
Syrus <syrusfarris@gmail.com>
2022-10-14 15:55:12 -0500

    created product tags model

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -7,6 +7,26 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allownull: true,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model:'product',
+        key:'id'
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGERS,
+      references: {
+        model:'tag',
+        key:'id'
+      }
+    }
   },
   {
     sequelize,
