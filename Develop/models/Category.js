970de34e8a13e76fa7c2002770bca292309ee497
Syrus <syrusfarris@gmail.com>
2022-10-14 15:33:39 -0500

    creted category models

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -6,16 +6,15 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
     id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
+      allownull: false,
       primaryKey: true,
       autoIncrement: true
     },
     category_name: {
       type: DataTypes.STRING,
-      allowNull: false
+      allownull: false
     }
   },
   {
